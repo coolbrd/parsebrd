c122641a42138c42b2d73cb812040b443dba221c
Ian <ianthomasevistoN@gmail.com>
2021-02-01 16:59:25 -0500

    Initial commit

---
+++ src/sum.ts
@@ -0,0 +1,3 @@
+export default function sum(x1: number, x2: number): number {
+    return x1 + x2;
+}
---
+++ test/sum.test.ts
@@ -0,0 +1,5 @@
+import sum from "../src/sum";
+
+test("sum", () => {
+    expect(sum(1, 1)).toEqual(2);
+});
