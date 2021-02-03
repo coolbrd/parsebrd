5940ddde5eb1f193076c1aa756922472bcc34057
Ian <ianthomasevistoN@gmail.com>
2021-02-03 17:04:43 -0500

    Added basic text parsing functionality

--- src/parsebrd.ts
+++ src/parsebrd.ts
@@ -1,3 +1,99 @@
-export default class Parsebrd {
+import { stripIndent } from "common-tags";
 
+interface ParsebrdArgument {
+    text: string
+}
+
+export default class Parsebrd {
+    public readonly originalText: string;
+    public readonly originalArguments: ParsebrdArgument[];
+
+    private readonly currentArguments: ParsebrdArgument[];
+
+    constructor(text: string, options?: { prefix?: string }) {
+        this.originalText = text;
+
+        if (options && options.prefix) {
+            text = this.removePrefix(text, options.prefix);
+        }
+
+        this.originalArguments = this.textToArguments(text);
+        this.currentArguments = Array.from(this.originalArguments);
+    }
+
+    private removePrefix(text: string, prefix: string): string {
+        if (!text.startsWith(prefix)) {
+            throw new Error(stripIndent`
+                Parsebrd was given text that did not start with the indicated prefix.
+
+                Text: ${text}
+                Prefix: ${prefix}
+            `);
+        }
+
+        return text.replace(prefix, "");
+    }
+
+    private splitByQuotesAndSpaces(text: string): string[] {
+        const splitByQuotes = text.split(`"`);
+        
+        let splitByQuotesAndSpaces: string[] = [];
+        for (let i = 0; i < splitByQuotes.length; i++) {
+            let currentText = splitByQuotes[i].trim();
+
+            if (i % 2 == 0) {
+                splitByQuotesAndSpaces.push(...currentText.split(" "));
+            }
+            else {
+                splitByQuotesAndSpaces.push(currentText);
+            }
+        }
+
+        this.truncateEmptyStrings(splitByQuotesAndSpaces);
+
+        return splitByQuotesAndSpaces;
+    }
+
+    private truncateEmptyStrings(elements: string[]): void {
+        if (elements.length > 0 && elements[0] === "") {
+            elements.shift();
+        }
+
+        if (elements.length > 0 && elements[elements.length - 1] === "") {
+            elements.pop();
+        }
+    }
+
+    private stringArrayToArguments(elements: string[]): ParsebrdArgument[] {
+        return elements.map(text => { return { text: text } });
+    }
+
+    private textToArguments(text: string): ParsebrdArgument[] {
+        const textArguments = this.splitByQuotesAndSpaces(text);
+
+        return this.stringArrayToArguments(textArguments);
+    }
+
+    public get argumentsRemaining(): number {
+        return this.currentArguments.length;
+    }
+
+    public get hasNextArgument(): boolean {
+        if (this.argumentsRemaining > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public nextArgument(): ParsebrdArgument {
+        if (!this.hasNextArgument) {
+            throw new Error("Parsebrd attempted to get the next argument from an array of none.");
+        }
+
+        const nextArgument = this.currentArguments.shift() as ParsebrdArgument;
+
+        return nextArgument;
+    }
 }
--- test/root/parsebrd.test.ts
+++ test/root/parsebrd.test.ts
@@ -1,13 +1,83 @@
 import Parsebrd from "../../src/parsebrd";
 
-describe("command parser", () => {
-    let parsebrd: Parsebrd;
+describe("parsebrd initialization", () => {
+    it("should properly split normal text", () => {
+        const parsebrd = new Parsebrd("some command text");
 
-    beforeEach(() => {
-        parsebrd = new Parsebrd();
+        expect(parsebrd.originalText).toBe("some command text");
+        expect(parsebrd.originalArguments.length).toBe(3);
+
+        expect(parsebrd.originalArguments[0].text).toBe("some");
+        expect(parsebrd.originalArguments[1].text).toBe("command");
+        expect(parsebrd.originalArguments[2].text).toBe("text");
     });
 
-    it("should exist", () => {
-        expect(parsebrd).toBeDefined();
+    it("should properly split quoted text", () => {
+        let parsebrd = new Parsebrd(`"extremely quoted" text with "lots" of "" abnormalities" and words "!`);
+        expect(parsebrd.originalArguments.length).toBe(9);
+        expect(parsebrd.originalArguments[0].text).toBe("extremely quoted");
+        expect(parsebrd.originalArguments[1].text).toBe("text");
+        expect(parsebrd.originalArguments[2].text).toBe("with");
+        expect(parsebrd.originalArguments[3].text).toBe("lots");
+        expect(parsebrd.originalArguments[4].text).toBe("of");
+        expect(parsebrd.originalArguments[5].text).toBe("");
+        expect(parsebrd.originalArguments[6].text).toBe("abnormalities");
+        expect(parsebrd.originalArguments[7].text).toBe("and words");
+        expect(parsebrd.originalArguments[8].text).toBe("!");
+
+        parsebrd = new Parsebrd(`this one "starts without quoted text" but "ends with some"`);
+        expect(parsebrd.originalArguments.length).toBe(5);
+        expect(parsebrd.originalArguments[0].text).toBe("this");
+        expect(parsebrd.originalArguments[1].text).toBe("one");
+        expect(parsebrd.originalArguments[2].text).toBe("starts without quoted text");
+        expect(parsebrd.originalArguments[3].text).toBe("but");
+        expect(parsebrd.originalArguments[4].text).toBe("ends with some");
+
+        parsebrd = new Parsebrd(`"just one big quote argument"`);
+        expect(parsebrd.originalArguments.length).toBe(1);
+        expect(parsebrd.originalArguments[0].text).toBe("just one big quote argument");
+
+        parsebrd = new Parsebrd(`just one "quote in a weird spot`);
+        expect(parsebrd.originalArguments.length).toBe(3);
+        expect(parsebrd.originalArguments[0].text).toBe("just");
+        expect(parsebrd.originalArguments[1].text).toBe("one");
+        expect(parsebrd.originalArguments[2].text).toBe("quote in a weird spot");
+
+        parsebrd = new Parsebrd(`"at the beginning`);
+        expect(parsebrd.originalArguments.length).toBe(1);
+        expect(parsebrd.originalArguments[0].text).toBe("at the beginning");
+
+        parsebrd = new Parsebrd("");
+        expect(parsebrd.originalArguments.length).toBe(0);
+
+        parsebrd = new Parsebrd(`""`);
+        expect(parsebrd.originalArguments.length).toBe(1);
+        expect(parsebrd.originalArguments[0].text).toBe("");
     });
-})
+
+    it("should properly remove prefixes", () => {
+        let parsebrd = new Parsebrd("b/hello, beasiary command!", { prefix: "b/" });
+        expect(parsebrd.originalArguments[0].text.startsWith("b/")).toBe(false);
+
+        expect(() => {
+            new Parsebrd("hello, not a command!", { prefix: "b/" });
+        }).toThrow();
+
+        parsebrd = new Parsebrd(" command with prefix of a space", { prefix: " "});
+        expect(parsebrd.originalArguments[0].text.startsWith(" ")).toBe(false);
+    });
+
+    it("should properly iterate arguments", () => {
+        const parsebrd = new Parsebrd("a few arguments");
+
+        expect(parsebrd.argumentsRemaining).toBe(3);
+        expect(parsebrd.hasNextArgument).toBe(true);
+
+        expect(parsebrd.nextArgument().text).toBe("a");
+        expect(parsebrd.nextArgument().text).toBe("few");
+        expect(parsebrd.nextArgument().text).toBe("arguments");
+
+        expect(parsebrd.hasNextArgument).toBe(false);
+        expect(() => parsebrd.nextArgument()).toThrow();
+    });
+});
