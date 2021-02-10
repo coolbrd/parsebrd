5248da48f7d20c68fea99c3d897ffa1f5bd1aef4
Ian <ianthomasevistoN@gmail.com>
2021-02-10 17:25:11 -0500

    Added user fetching testing

--- test/root/parsebrd-discord.test.ts
+++ test/root/parsebrd-discord.test.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, User, UserManager } from "discord.js";
 import ParsebrdDiscord from "../../src/concrete/parsebrd-discord";
 
 jest.mock("discord.js");
@@ -7,6 +7,8 @@ describe("ParsebrdDiscord user id identification", () => {
     let mockedClient: Client;
 
     beforeEach(() => {
+        jest.resetAllMocks();
+
         mockedClient = new Client();
     });
 
@@ -40,5 +42,57 @@ describe("ParsebrdDiscord user id identification", () => {
 
         parsebrd = new ParsebrdDiscord("101010101o10101010", mockedClient);
         expect(parsebrd.nextArgument().userId).toBeUndefined();
+    });
+});
+
+describe("ParsebrdDiscord user loading", () => {
+    let mockedClient: Client;
+    let mockedUserManager: UserManager;
+    let mockedUserManagerFetch: jest.SpyInstance;
+
+    beforeEach(() => {
+        mockedClient = new Client();
+        mockedUserManager = new UserManager(mockedClient);
+        mockedUserManagerFetch = jest.spyOn(mockedUserManager, "fetch");
+
+        mockedClient.users = mockedUserManager;
+
+        mockedUserManagerFetch.mockResolvedValue(new User(mockedClient, {}));
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it("should attempt to load user ids", async () => {
+        const parsebrd = new ParsebrdDiscord("101010101010101010", mockedClient);
+        expect(mockedUserManager.fetch).not.toBeCalled();
+        await parsebrd.load();
+        expect(mockedUserManager.fetch).toBeCalledTimes(1);
+    });
+
+    it("should load users from ids", async () => {
+        const parsebrd = new ParsebrdDiscord("101010101010101010", mockedClient);
+        expect(parsebrd.originalArguments[0].user).toBeUndefined();
+        await parsebrd.load();
+        expect(parsebrd.originalArguments[0].user).toBeDefined();
+    });
+
+    it("should not assign users when fetch fails", async () => {
+        mockedUserManagerFetch.mockImplementation(async () => { throw new Error("Test error") });
+
+        const parsebrd = new ParsebrdDiscord("101010101010101010", mockedClient);
+        const userArgument = parsebrd.originalArguments[0];
+        userArgument.user = new User(mockedClient, {});
+        await parsebrd.load();
+
+        expect(userArgument.userId).toBeDefined();
+        expect(userArgument.user).toBe(undefined);
+    });
+
+    it("should not attempt to fetch a user from a non-id", async () => {
+        const parsebrd = new ParsebrdDiscord("no ids here", mockedClient);
+        await parsebrd.load();
+        expect(mockedUserManagerFetch).not.toBeCalled();
     });
 });
