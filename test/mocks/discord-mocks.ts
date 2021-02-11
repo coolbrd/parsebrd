e8c0a3efc8a48dd31e8f7a3144dfaf0a82d96fa9
Ian <ianthomasevistoN@gmail.com>
2021-02-11 13:57:48 -0500

    Added message mock creator

---
+++ test/mocks/discord-mocks.ts
@@ -0,0 +1,13 @@
+import { Channel, Client, DMChannel, Guild, Message, NewsChannel, TextChannel } from "discord.js";
+
+jest.mock("discord.js");
+
+export function createMockedMessage(options?: { client?: Client, data?: any, channel?: TextChannel | DMChannel | NewsChannel }): Message {
+    const mockedClient = options && options.client ? options.client : new Client();
+
+    const mockedChannel = options && options.channel ? options.channel : new TextChannel(new Guild(mockedClient, {}));
+
+    const mockedMessage = new Message(mockedClient, {}, mockedChannel);
+
+    return mockedMessage;
+}
