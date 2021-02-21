785c572cc375f247f3683b535fada468610f5e59
Ian <ianthomasevistoN@gmail.com>
2021-02-21 14:35:49 -0500

    Widened parameters

--- test/utility/mocks/discord-mocks.ts
+++ test/utility/mocks/discord-mocks.ts
@@ -1,15 +1,20 @@
-import { Client, DMChannel, Guild, Message, NewsChannel, TextChannel } from "discord.js";
+import { Client, DMChannel, Guild, GuildMember, Message, NewsChannel, TextChannel, User } from "discord.js";
 
 jest.mock("discord.js");
 
-export function createMockedMessage(options?: { client?: Client, data?: any, channel?: TextChannel | DMChannel | NewsChannel }): Message {
-    const mockedClient = options && options.client ? options.client : new Client();
-
-    const mockedChannel = options && options.channel ? options.channel : new TextChannel(new Guild(mockedClient, {}));
+export function createMockedMessage(options?: { client?: Client, data?: any, guild?: Guild, channel?: TextChannel | DMChannel | NewsChannel, author?: User, member?: GuildMember }): Message {
+    const mockedClient = options && options.client !== undefined ? options.client : new Client();
+    const mockedGuild = options && options.guild !== undefined ? options.guild : new Guild(mockedClient, {});
+    const mockedChannel = options && options.channel !== undefined ? options.channel : new TextChannel(mockedGuild);
+    const mockedUser = options && options.author !== undefined ? options.author : new User(mockedClient, {});
+    const mockedMember = options && options.member !== undefined ? options.member : new GuildMember(mockedClient, {}, mockedGuild);
 
     const mockedMessage = new Message(mockedClient, {}, mockedChannel);
     (mockedMessage as any).client = mockedClient;
-    mockedMessage.channel = mockedChannel;
+    (mockedMessage as any).guild = mockedGuild;
+    (mockedMessage as any).channel = mockedChannel;
+    (mockedMessage as any).author = mockedUser;
+    (mockedMessage as any).member = mockedMember;
 
     return mockedMessage;
 }
